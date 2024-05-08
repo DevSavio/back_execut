0d51da6afe5e98bd5be1c12984517a912fd8938a
DevSavio <develop.savio@gmail.com>
2024-05-08 12:28:48 -0300

    Adicionado o import do tempo de obra

--- bin/execut.dart
+++ bin/execut.dart
@@ -9,7 +9,7 @@ import 'package:shelf/shelf_io.dart' as io;
 // import 'obra/obra_controller.dart';
 // import 'usuario/usuario_controller.dart';
 // import 'tipoMaoDeObra/tipoMaoDeObra_controller.dart';
-import 'tipo_mao_de_obra/tipo_mao_de_obra_controller.dart';
+// import 'tempo_de_obra/tempo_de_obra_controller.dart';
 
 void main() async {
 
@@ -17,29 +17,7 @@ void main() async {
   //tipoMaoDeObra
   //tempoDeObra
 
-  TempoDeObraController tempoDeObraController = TempoDeObraController();
-
-    tempoDeObraController.create(
-       nomeTempo: "Curto prazo",
-    );
-
-    tempoDeObraController.readByID(
-      idTempo: 1,
-    );
-
-    tempoDeObraController.update(
-      nomeTempo: "",
-      idTempo: 1,
-    );
-
-    tempoDeObraController.delete(
-      idTempo: 1,
-    );
-
-    tempoDeObraController.list();
-
-    tempoDeObraController.search(
-        operator: "like", value: "'%Ex%'", paramter: "nomeCliente");
+  
 
 }
 
