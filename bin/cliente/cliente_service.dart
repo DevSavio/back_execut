101962e086ce0bed10322a2773b87bb32469cc27
DevSavio <develop.savio@gmail.com>
2024-05-18 17:58:17 -0300

    Retirado as aspas simples dos valores do json

--- bin/cliente/cliente_service.dart
+++ bin/cliente/cliente_service.dart
@@ -8,11 +8,11 @@ class ClienteService {
 
   bool criarClient() {
     clienteController.create(
-      complemento: "'Apto 45'",
+      complemento: "Apto 45",
       cpfCnpj: "12345678901234",
-      logradouro: "'Rua das Flores, 123'",
-      nomeCliente: "'João Silva'",
-      razaoSocial: "'JS Serviços'",
+      logradouro: "Rua das Flores, 123",
+      nomeCliente: "João Silva",
+      razaoSocial: "JS Serviços",
       telefone: "11987654321",
     );
     return true;
@@ -30,11 +30,11 @@ class ClienteService {
 
   bool atualizarCliente() {
     clienteController.update(
-      complemento: "'Apto 1010'",
+      complemento: "Apto 1010",
       cpfCnpj: "12345678901234",
-      logradouro: "'Rua das Flores, 123'",
-      nomeCliente: "'João Silva'",
-      razaoSocial: "'JS Serviços'",
+      logradouro: "Rua das Flores, 123",
+      nomeCliente: "João Silva",
+      razaoSocial: "JS Serviços",
       telefone: "11987654321",
       idCliente: 8,
     );
