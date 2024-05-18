d682c9250ea55fc1b5d7136eed73a09043fe0e58
Thiago Augusto <wstagf@gmail.com>
2024-05-18 16:41:50 -0300

    buscando por id

--- bin/execut.dart
+++ bin/execut.dart
@@ -3,8 +3,6 @@ import 'dart:io';
 import 'package:shelf_plus/shelf_plus.dart';
 import 'cliente/cliente_service.dart';
 
-import 'person.dart';
-
 void main() => shelfRun(init);
 
 Handler init() {
@@ -13,55 +11,62 @@ Handler init() {
   /// Serve index page of frontend
   app.get('/', () => File('frontend/page.html'));
 
-  /// List all persons
+  /// List all cliente
   app.get('/clientes', () {
     ClienteService c = ClienteService();
     final data = c.listarClientes();
     return data;
   });
 
-  /// Get specific person by id
-  app.get('/person/<id>', (Request request, String id) {
-    final data = <Person>[
-      Person(firstName: 'John', lastName: 'Doe', age: 42),
-      Person(firstName: 'Jane', lastName: 'Doe', age: 43),
-    ];
-    return data.where((person) => person.id == id);
+  /// Get specific cliente by id
+  app.get('/clientes/<id>', (Request request, String id) {
+    ClienteService c = ClienteService();
+    final data = c.buscarCliente(int.parse(id));
+    return data;
   });
 
-  /// Add a new person
-  app.post('/person', (Request request) async {
-    var newPerson = await request.body.as(Person.fromJson);
-    final data = <Person>[
-      Person(firstName: 'John', lastName: 'Doe', age: 42),
-      Person(firstName: 'Jane', lastName: 'Doe', age: 43),
-    ];
-    data.add(newPerson);
-    return {'ok': 'true', 'person': newPerson.toJson()};
-  });
+  // ///
+  // app.get('/person/<id>', (Request request, String id) {
+  //   final data = <Person>[
+  //     Person(firstName: 'John', lastName: 'Doe', age: 42),
+  //     Person(firstName: 'Jane', lastName: 'Doe', age: 43),
+  //   ];
+  //   return data.where((person) => person.id == id);
+  // });
 
-  /// Update an existing person by id
-  app.put('/person/<id>', (Request request, String id) async {
-    final data = <Person>[
-      Person(firstName: 'John', lastName: 'Doe', age: 42),
-      Person(firstName: 'Jane', lastName: 'Doe', age: 43),
-    ];
-    data.removeWhere((person) => person.id == id);
-    var person = await request.body.as(Person.fromJson);
-    person.id = id;
-    data.add(person);
-    return {'ok': 'true'};
-  });
+  // /// Add a new person
+  // app.post('/person', (Request request) async {
+  //   var newPerson = await request.body.as(Person.fromJson);
+  //   final data = <Person>[
+  //     Person(firstName: 'John', lastName: 'Doe', age: 42),
+  //     Person(firstName: 'Jane', lastName: 'Doe', age: 43),
+  //   ];
+  //   data.add(newPerson);
+  //   return {'ok': 'true', 'person': newPerson.toJson()};
+  // });
 
-  /// Remove a specific person by id
-  app.delete('/person/<id>', (Request request, String id) {
-    final data = <Person>[
-      Person(firstName: 'John', lastName: 'Doe', age: 42),
-      Person(firstName: 'Jane', lastName: 'Doe', age: 43),
-    ];
-    data.removeWhere((person) => person.id == id);
-    return {'ok': 'true'};
-  });
+  // /// Update an existing person by id
+  // app.put('/person/<id>', (Request request, String id) async {
+  //   final data = <Person>[
+  //     Person(firstName: 'John', lastName: 'Doe', age: 42),
+  //     Person(firstName: 'Jane', lastName: 'Doe', age: 43),
+  //   ];
+  //   data.removeWhere((person) => person.id == id);
+  //   var person = await request.body.as(Person.fromJson);
+  //   person.id = id;
+  //   data.add(person);
+  //   return {'ok': 'true'};
+  // });
+
+  // /// Remove a specific person by id
+  // app.delete('/person/<id>', (Request request, String id) {
+  //   final data = <Person>[
+  //     Person(firstName: 'John', lastName: 'Doe', age: 42),
+  //     Person(firstName: 'Jane', lastName: 'Doe', age: 43),
+  //   ];
+  //   data.removeWhere((person) => person.id == id);
+  //   return {'ok': 'true'};
+  // });
 
   return app;
 }
