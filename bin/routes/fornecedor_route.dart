90d408b3d91bb1592d86b970da8a971a519b6ad0
DevSavio <develop.savio@gmail.com>
2024-05-18 22:26:54 -0300

    Adicionado ao projeto a rota do modulo fornecedor

--- bin/routes/fornecedor_route.dart
+++ bin/routes/fornecedor_route.dart
@@ -1,6 +1,6 @@
 import 'package:shelf_plus/shelf_plus.dart';
 import "../fornecedor/fornecedor_service.dart";
-import "../models/cliente_models.dart";
+import '../models/fornecedor_models.dart';
 
 class FornecedorRoute {
   RouterPlus router = RouterPlus();
@@ -11,104 +11,102 @@ class FornecedorRoute {
   RouterPlus init() {
     /// List all
     router.get('/fornecedores', () {
-      final data = fornecedorService.listar();
-      return {
-        'ok': 'true - fornecedores',
-      };
+      final data = fornecedorService.listarFornecedores();
+      return data;
     });
 
-    /// Get specific cliente by id
-    router.get('/cliente2/<id>', (Request request, String id) {
-      // final data = clienteService.buscarCliente(int.parse(id));
-      // return data;
+    /// Get specific fornecedor by id
+    router.get('/fornecedor2/<id>', (Request request, String id) {
+      final data = fornecedorService.buscarFornecedor(int.parse(id));
+      return data;
     });
 
-    /// Add a new clientes
-    router.post('/cliente', (Request request) async {
+    /// Add a new fornecedor
+    router.post('/fornecedor', (Request request) async {
       try {
-        // dynamic body = await request.body.asJson;
+        dynamic body = await request.body.asJson;
 
-        // ClienteModel novoCliente = ClienteModel.fromJson(body);
+        FornecedorModel novoFornecedor = FornecedorModel.fromJson(body);
 
-        // ClienteModel? clienteAdicionado =
-        //     await clienteService.criarClient(clienteModel: novoCliente);
+        FornecedorModel? fornecedorAdicionado =
+            await fornecedorService.criarFornecedor(fornecedorModel: novoFornecedor);
 
-        // if (clienteAdicionado != null) {
-        //   return {
-        //     'ok': 'true',
-        //     'clienteAdicionado': clienteAdicionado,
-        //   };
-        // }
+        if (fornecedorAdicionado != null) {
+          return {
+            'ok': 'true',
+            'fornecedorAdicionado': fornecedorAdicionado,
+          };
+        }
+      } catch (e) {
+        return Response.internalServerError(body: 'Erro ao criar fornecedor');
+      }
+    });
+
+    /// delete a new fornecedor
+    router.delete('/fornecedor/<id>', (Request request, String id) async {
+      try {
+        var objetoRetorno = await fornecedorService
+            .deletarFornecedor(int.parse(request.params['id']!));
+        print(objetoRetorno);
+
+        if (objetoRetorno != null) {
+          return {
+            'ok': 'true',
+            'fornecedor_excluido ': objetoRetorno,
+          };
+        } else {
+          return {
+            'ok': 'false',
+            'fornecedor_excluido': "Fornecedor não encontrado",
+          };
+        }
+      } catch (e) {
+        return Response.internalServerError(body: 'Erro ao criar fornecedor');
+      }
+    });
+
+    /// update a fornecedor
+    router.post('/fornecedor/<id>', (Request request) async {
+      try {
+        dynamic body = await request.body.asJson;
+
+        FornecedorModel novoFornecedor = FornecedorModel.fromJson(body);
+
+        var fornecedorAtualizado = await fornecedorService.atualizarFornecedor(
+          fornecedorModel: novoFornecedor,
+          idFornecedor: int.parse(
+            request.params['id']!,
+          ),
+        );
+
+        if (fornecedorAtualizado != null) {
+          return {
+            'ok': 'true',
+            'fornecedorAlterado': fornecedorAtualizado,
+          };
+        }
       } catch (e) {
         return Response.internalServerError(body: 'Erro ao criar cliente');
       }
     });
 
-    /// delete a new cliente
-    router.delete('/cliente/<id>', (Request request, String id) async {
-      try {
-        // var objetoRetorno = await clienteService
-        //     .deletarCliente(int.parse(request.params['id']!));
-        // print(objetoRetorno);
-
-        // if (objetoRetorno != null) {
-        //   return {
-        //     'ok': 'true',
-        //     'cliente_excluido ': objetoRetorno,
-        //   };
-        // } else {
-        //   return {
-        //     'ok': 'false',
-        //     'cliente_excluido': "Cliente não encontrado",
-        //   };
-        // }
-      } catch (e) {
-        return Response.internalServerError(body: 'Erro ao criar cliente');
-      }
-    });
-
-    /// update a clientes
-    router.post('/cliente/<id>', (Request request) async {
-      try {
-        // dynamic body = await request.body.asJson;
-
-        // ClienteModel novoCliente = ClienteModel.fromJson(body);
-
-        // var clientAtualizado = await clienteService.atualizarCliente(
-        //   clienteModel: novoCliente,
-        //   idCliente: int.parse(
-        //     request.params['id']!,
-        //   ),
-        // );
-
-        // if (clientAtualizado != null) {
-        //   return {
-        //     'ok': 'true',
-        //     'clienteAlterado': clientAtualizado,
-        //   };
-        // }
-      } catch (e) {
-        return Response.internalServerError(body: 'Erro ao criar cliente');
-      }
-    });
-
-    /// buscar clientes por qualquer campo
+    /// buscar fornecedor por qualquer campo
     router.get('/cliente/buscar_por_campo', (Request request) async {
       try {
-        // dynamic body = await request.body.asJson;
+        dynamic body = await request.body.asJson;
 
-        // var clientAtualizado = await clienteService.buscarClientePorNome(
-        //   operator: body['operator'],
-        //   paramter: body['paramter'],
-        //   value: body['value'],
-        // );
+        var fornecedorAtualizado = await fornecedorService.buscarFornecedorPorNome(
+          operator: body['operator'],
+          paramter: body['paramter'],
+          value: body['value'],
+        );
 
-        // if (clientAtualizado != null) {
-        //   return {
-        //     'ok': 'true',
-        //     'clienteAlterado': clientAtualizado,
-        //   };
-        // }
+        if (fornecedorAtualizado != null) {
+          return {
+            'ok': 'true',
+            'clienteAlterado': fornecedorAtualizado,
+          };
+        }
       } catch (e) {
         return Response.internalServerError(body: 'Erro ao criar cliente');
       }
