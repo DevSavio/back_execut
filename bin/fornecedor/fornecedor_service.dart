ce06f516fafd192fba075b5e37d19795f513c4e5
DevSavio <develop.savio@gmail.com>
2024-05-18 22:26:18 -0300

    Implementado as funcionaliades do service fornecedor

--- bin/fornecedor/fornecedor_service.dart
+++ bin/fornecedor/fornecedor_service.dart
@@ -1,20 +1,93 @@
+import '../models/fornecedor_models.dart';
 import 'fornecedor_controller.dart';
 
 class FornecedorService {
   FornecedorController fornecedorController = FornecedorController();
-  void cadastrar() {
-    print('Cadastrando fornecedor');
+
+  Future<FornecedorModel?> criarFornecedor(
+    {required FornecedorModel fornecedorModel}) async {
+    try {
+      int? insertedID = await fornecedorController.create(
+        complemento: fornecedorModel.complemento ?? "",
+        logradouro: fornecedorModel.logradouro,
+        razaoSocial: fornecedorModel.razaoSocial,
+        telefone: fornecedorModel.telefone,
+        idTipo: fornecedorModel.idTipo,
+      );
+
+      if (insertedID != null) {
+        FornecedorModel? fornecedorModel =
+            await fornecedorController.readByID(idFornecedor: insertedID);
+        return fornecedorModel;
+      } else {
+        throw Exception("Erro ao criar fornecedor");
+      }
+    } catch (e) {
+      throw Exception("Erro ao criar fornecedor");
+    }
   }
 
-  void editar() {
-    print('Editando fornecedor');
+  Future<bool> atualizarFornecedor(
+    {required FornecedorModel fornecedorModel, required int idFornecedor}) async {
+    try {
+      var result = await fornecedorController.update(
+        complemento: fornecedorModel.complemento ?? "",
+        razaoSocial: fornecedorModel.razaoSocial, 
+        logradouro: fornecedorModel.logradouro, 
+        telefone: fornecedorModel.telefone, 
+        idTipo: fornecedorModel.idTipo, 
+        idFornecedor: idFornecedor,
+        );
+
+        if (result) {
+        return false;
+      } else {
+        return true;
+      }
+    } catch (e) {
+      rethrow;
+    }
   }
 
-  void listar() {
-    print('Listando fornecedores');
+  Future<List<FornecedorModel>> listarFornecedores() async{
+    try {
+      return await fornecedorController.list();
+    } catch (e) {
+      rethrow;
+    }
   }
 
-  void deletar() {
-    print('Deletando fornecedor');
+  Future<FornecedorModel?> buscarFornecedor(int idFornecedor) {
+    try {
+      return fornecedorController.readByID(
+        idFornecedor: idFornecedor,
+      );
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+   dynamic buscarFornecedorPorNome({
+    required String operator,
+    required String value,
+    required String paramter,
+  }) {
+    return fornecedorController.search(
+        operator: operator, value: value, paramter: paramter);
+  }
+
+  Future<int?> deletarFornecedor(int idFornecedor) async {
+    try {
+      int? id = await fornecedorController.delete(
+        idFornecedor: idFornecedor,
+      );
+      if (id != null) {
+        return id;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      throw Exception("Erro ao excluir fornecedor ");
+    }
   }
 }
