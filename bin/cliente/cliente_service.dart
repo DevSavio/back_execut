027905bd68209e7661a8e57f53a0a48887f93265
DevSavio <develop.savio@gmail.com>
2024-05-24 19:00:53 -0300

    arrumando a ordem da resposta do booleano para update
    Co-authored-by: Thiago Augusto <wstagf@gmail.com>

--- bin/cliente/cliente_service.dart
+++ bin/cliente/cliente_service.dart
@@ -52,9 +52,9 @@ class ClienteService {
       );
 
       if (reult) {
-        return false;
-      } else {
         return true;
+      } else {
+        return false;
       }
     } catch (e) {
       rethrow;
