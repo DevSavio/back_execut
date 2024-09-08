51f9392d794adaafcda36f28f26f6d799e72be81
DevSavio <develop.savio@gmail.com>
2024-09-08 10:01:43 -0300

    Retirado codigo para mock do cliente

--- bin/execut.dart
+++ bin/execut.dart
@@ -47,28 +47,5 @@ Handler init() {
   app = TipoMaoDeObraRoute(router: app).init();
   app = UsuarioRoute(router: app).init();
 
-  // /// Update an existing person by id
-  // app.put('/person/<id>', (Request request, String id) async {
-  //   final data = <Person>[
-  //     Person(firstName: 'John', lastName: 'Doe', age: 42),
-  //     Person(firstName: 'Jane', lastName: 'Doe', age: 43),
-  //   ];
-  //   data.removeWhere((person) => person.id == id);
-  //   var person = await request.body.as(Person.fromJson);
-  //   person.id = id;
-  //   data.add(person);
-  //   return {'ok': 'true'};
-  // });
-
-  // /// Remove a specific person by id
-  // app.delete('/person/<id>', (Request request, String id) {
-  //   final data = <Person>[
-  //     Person(firstName: 'John', lastName: 'Doe', age: 42),
-  //     Person(firstName: 'Jane', lastName: 'Doe', age: 43),
-  //   ];
-  //   data.removeWhere((person) => person.id == id);
-  //   return {'ok': 'true'};
-  // });
-
   return app;
 }
